694f9ec7b49875eaf9808e529e83d98ca2ceac67
alanskorka <alansko2002@gmail.com>
2025-10-22 19:56:33 +0100

    feat(auth): GitHub OAuth working locally

---
+++ backend/src/main/java/iscte/gitstats/GitstatsApplication.java
@@ -0,0 +1,13 @@
+package iscte.gitstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitstatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitstatsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/iscte/gitstats/GitstatsApplicationTests.java
@@ -0,0 +1,13 @@
+package iscte.gitstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitstatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
