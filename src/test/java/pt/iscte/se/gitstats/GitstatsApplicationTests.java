8f0cb24c11893e37a8d0d02a6e598574df943349
Kyan ZEYNALI <kyan.zeynali@edu.esiee.fr>
2025-10-24 16:53:47 +0100

    Authentification structure fixed

--- src/test/java/pt/iscte/se/gitstats/GitstatsApplicationTests.java
+++ src/test/java/pt/iscte/se/gitstats/GitstatsApplicationTests.java
@@ -1,4 +1,4 @@
-package iscte.gitstats;
+package pt.iscte.se.gitstats;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GitstatsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {}
 
 }
